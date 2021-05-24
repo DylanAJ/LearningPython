81d02757a59454792779b1220fc9f05118d50f65
DylanAJ <72707390+DylanAJ@users.noreply.github.com>
2021-05-24 13:45:48 +0100

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,7 +1,7 @@
 
 num1 = float(input("Enter first number: "))
 op = input("Enter operator: ")
-num2 = float(input("Enter first number: "))
+num2 = float(input("Enter second numer: "))
 
 if op == "+":
     print(num1 + num2)
